c125d06666f7cc6b60f7352ebff81843ce5b2e51
malawad <50057882+malawadd@users.noreply.github.com>
2025-07-01 19:19:31 +0300

    Update TomoProvider.tsx

--- src/TomoProvider.tsx
+++ src/TomoProvider.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { getDefaultConfig, TomoEVMKitProvider } from '@tomo-inc/tomo-evm-kit';
 import { metaMaskWallet, rainbowWallet, walletConnectWallet } from '@tomo-inc/tomo-evm-kit/wallets';
 import { WagmiProvider } from 'wagmi';
-import { storyAeneid, story } from 'wagmi/chains';
+import { filecoin  , filecoinCalibration  } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const config = getDefaultConfig({
   clientId: '9TJ5ODBE68qK0yY7F8FSn5cScSdZZ1btWx3hxKlwYAlsaTr91H2EIR7CtoP7Ap0Il6h2GHmkXqIYD92o3E2QqZyP',
   appName: 'Kiyan',
   projectId: 'c4f79cc821944d9680842e34466bfbd', // Updated with a valid WalletConnect project ID format
-  chains: [storyAeneid, story],
+  chains: [filecoinCalibration, filecoin],
   ssr: false,
   wallets: [
     {
